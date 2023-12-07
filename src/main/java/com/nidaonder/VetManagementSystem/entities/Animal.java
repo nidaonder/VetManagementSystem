69b2a68291a32c086bf7e5da37e807ab9ef147c1
nidaonder <nidaaonder@gmail.com>
2023-12-07 15:47:16 +0300

    Animal entity added.

---
+++ src/main/java/com/nidaonder/VetManagementSystem/entities/Animal.java
@@ -0,0 +1,47 @@
+package com.nidaonder.VetManagementSystem.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "animals")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Animal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "animal_id", columnDefinition = "serial")
+    private long id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String species;
+
+    @NotBlank
+    private String breed;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+    public enum Gender {
+        MALE,
+        FEMALE
+    }
+
+    @NotBlank
+    private String colour;
+
+    private LocalDate dateOfBirth;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id", referencedColumnName = "customer_id")
+    private Customer customer;
+}
