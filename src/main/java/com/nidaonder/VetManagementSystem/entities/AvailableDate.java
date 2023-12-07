a5875fa091939c1fc281e1f05bb23e78bdc23dab
nidaonder <nidaaonder@gmail.com>
2023-12-07 16:45:54 +0300

    Entities added.

--- src/main/java/com/nidaonder/VetManagementSystem/entities/AvailableDate.java
+++ src/main/java/com/nidaonder/VetManagementSystem/entities/AvailableDate.java
@@ -1,7 +1,6 @@
 package com.nidaonder.VetManagementSystem.entities;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
