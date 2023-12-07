eb0248ab0998ec6196063365a6e9f92c9eb85360
nidaonder <nidaaonder@gmail.com>
2023-12-07 22:34:06 +0300

    Add Result class - Implemented a new Result class for handling operation results - Provides standardized format for success and error responses - Enhances consistency in response handling across the application

---
+++ src/main/java/com/nidaonder/VetManagementSystem/core/result/Result.java
@@ -0,0 +1,16 @@
+package com.nidaonder.VetManagementSystem.core.result;
+
+import lombok.Getter;
+
+@Getter
+public class Result {
+    private boolean status;
+    private String message;
+    private String httpCode;
+
+    public Result(boolean status, String message, String httpCode) {
+        this.status = status;
+        this.message = message;
+        this.httpCode = httpCode;
+    }
+}
