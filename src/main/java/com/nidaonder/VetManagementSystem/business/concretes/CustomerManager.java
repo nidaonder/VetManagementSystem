e47fe50e21fb78b3c80fb60dc8493e2bc43c1d36
nidaonder <nidaaonder@gmail.com>
2023-12-07 22:29:20 +0300

    CustomerManager added.

--- src/main/java/com/nidaonder/VetManagementSystem/business/concretes/CustomerManager.java
+++ src/main/java/com/nidaonder/VetManagementSystem/business/concretes/CustomerManager.java
@@ -1,14 +1,19 @@
 package com.nidaonder.VetManagementSystem.business.concretes;
 
 import com.nidaonder.VetManagementSystem.business.abstracts.ICustomerService;
+import com.nidaonder.VetManagementSystem.core.exception.DataExistsException;
+import com.nidaonder.VetManagementSystem.core.exception.NotFoundException;
+import com.nidaonder.VetManagementSystem.core.utilities.Msg;
 import com.nidaonder.VetManagementSystem.dao.CustomerRepo;
 import com.nidaonder.VetManagementSystem.dto.request.CustomerRequest;
 import com.nidaonder.VetManagementSystem.dto.response.CustomerResponse;
+import com.nidaonder.VetManagementSystem.entities.Customer;
 import com.nidaonder.VetManagementSystem.mapper.CustomerMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -24,12 +29,17 @@ public class CustomerManager implements ICustomerService {
 
     @Override
     public CustomerResponse getById(long id) {
-        return customerMapper.asOutput(customerRepo.findById(id).orElseThrow(() -> new RuntimeException("Customer not found!")));
+        return customerMapper.asOutput(customerRepo.findById(id).orElseThrow(() -> new NotFoundException(Msg.NOT_FOUND)));
     }
 
     @Override
     public CustomerResponse create(CustomerRequest request) {
-        return null;
+        Optional<Customer> isCustomerExist = customerRepo.findByMail(request.getMail());
+        if (isCustomerExist.isEmpty()){
+            Customer customerSaved = customerRepo.save(customerMapper.asEntity(request));
+            return customerMapper.asOutput(customerSaved);
+        }
+        throw new DataExistsException(Msg.DATA_EXISTS);
     }
 
     @Override
@@ -39,6 +49,11 @@ public class CustomerManager implements ICustomerService {
 
     @Override
     public void deleteById(long id) {
-
+        Optional<Customer> customerFromDb = customerRepo.findById(id);
+        if (customerFromDb.isPresent()){
+            customerRepo.delete(customerFromDb.get());
+        } else {
+            throw new NotFoundException(Msg.NOT_FOUND);
+        }
     }
 }
