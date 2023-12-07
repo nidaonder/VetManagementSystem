82d146e536d3bdcb0539886cb810a50e2705a54c
nidaonder <nidaaonder@gmail.com>
2023-12-07 15:54:28 +0300

    AvailableDate entity added.

---
+++ src/main/java/com/nidaonder/VetManagementSystem/entities/AvailableDate.java
@@ -0,0 +1,29 @@
+package com.nidaonder.VetManagementSystem.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "available_date")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AvailableDate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "available_date_id", columnDefinition = "serial")
+    private long id;
+
+    @NotNull
+    private LocalDate appointmentDate;
+
+    @ManyToOne
+    @JoinColumn(name = "doctor_id", referencedColumnName = "doctor_id")
+    private Doctor doctor;
+}
