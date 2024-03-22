31f8c4766b16747144871eaefdcf185063641c0a
nidaonder <nidaaonder@gmail.com>
2024-03-22 16:02:16 +0300

    updated webconfig

--- src/main/java/com/nidaonder/VetManagementSystem/config/WebConfig.java
+++ src/main/java/com/nidaonder/VetManagementSystem/config/WebConfig.java
@@ -10,10 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-//                        "https://vetapp-frontend-kappa.vercel.app"
-                        "http://localhost:5173/"
-                )
+                .allowedOrigins( "http://localhost:5173/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
